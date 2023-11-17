4b329954ece55c01ca28a484f8e4fcfb93287629
user.email <evzhurilo@gmail.com>
2023-11-17 14:10:54 -0400

    add method search for setting and value of search string and sending ENTER key at the end

--- src/test/java/MainPage.java
+++ src/test/java/MainPage.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.checkerframework.checker.units.qual.N;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import static com.codeborne.selenide.Selenide.$x;
 
@@ -15,10 +16,20 @@ import static com.codeborne.selenide.Selenide.$x;
 public class MainPage {
     //creating locator
     private final SelenideElement searchButton = $x("//form");
+    private final SelenideElement textBoxInput = $x("//input[@type='text']");
     private String url;
 
     public void clickOnSearch() {
         searchButton.click();
+    }
+
+    /**
+     * Making search between articles and pushing ENTER button
+     * @param searchText
+     * */
+    public void search(String searchText) {
+        textBoxInput.setValue(searchText);
+        textBoxInput.sendKeys(Keys.ENTER);
     }
 
     public void openWebSite(String url) {
